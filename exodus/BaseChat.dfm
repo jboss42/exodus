a87b48b787a5ca0b57675edfd81d5f220ff1ab1e
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2002-03-08 06:30:20 +0000

    start to formalize a base class for chat/room windows

---
+++ exodus/BaseChat.dfm
@@ -0,0 +1,101 @@
+object frmBaseChat: TfrmBaseChat
+  Left = 234
+  Top = 143
+  Width = 394
+  Height = 319
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Splitter1: TSplitter
+    Left = 0
+    Top = 251
+    Width = 386
+    Height = 3
+    Cursor = crVSplit
+    Align = alBottom
+    AutoSnap = False
+    Beveled = True
+  end
+  object Panel3: TPanel
+    Left = 0
+    Top = 22
+    Width = 386
+    Height = 229
+    Align = alClient
+    BevelOuter = bvNone
+    BorderWidth = 4
+    Caption = 'pnlMsgs'
+    TabOrder = 0
+    object MsgList: TExRichEdit
+      Left = 4
+      Top = 4
+      Width = 378
+      Height = 221
+      Align = alClient
+      ReadOnly = True
+      ScrollBars = ssBoth
+      TabOrder = 0
+      OnURLClick = MsgListURLClick
+    end
+  end
+  object pnlInput: TPanel
+    Left = 0
+    Top = 254
+    Width = 386
+    Height = 31
+    Align = alBottom
+    BevelOuter = bvNone
+    BorderWidth = 2
+    TabOrder = 1
+    object MsgOut: TMemo
+      Left = 2
+      Top = 2
+      Width = 382
+      Height = 27
+      Align = alClient
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Verdana'
+      Font.Style = []
+      ParentFont = False
+      PopupMenu = popOut
+      TabOrder = 0
+      WantReturns = False
+    end
+  end
+  object Panel1: TPanel
+    Left = 0
+    Top = 0
+    Width = 386
+    Height = 22
+    Align = alTop
+    BevelOuter = bvNone
+    BorderWidth = 2
+    TabOrder = 2
+  end
+  object popOut: TPopupMenu
+    Left = 16
+    Top = 184
+    object Copy1: TMenuItem
+      Caption = 'Copy'
+    end
+    object CopyAll1: TMenuItem
+      Caption = 'Copy All'
+    end
+    object Clear1: TMenuItem
+      Caption = 'Clear'
+    end
+    object Emoticons1: TMenuItem
+      Caption = 'Emoticons'
+      ShortCut = 16453
+      OnClick = Emoticons1Click
+    end
+  end
+end
---
+++ exodus/BaseChat.pas
@@ -0,0 +1,126 @@
+unit BaseChat;
+{
+    Copyright 2002, Peter Millard
+
+    This file is part of Exodus.
+
+    Exodus is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Exodus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Exodus; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+interface
+
+uses
+    Dockable, 
+    Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+    Dialogs, Menus, StdCtrls, ExtCtrls, ComCtrls, OLERichEdit, ExRichEdit;
+
+type
+  TfrmBaseChat = class(TfrmDockable)
+    Panel3: TPanel;
+    MsgList: TExRichEdit;
+    Splitter1: TSplitter;
+    pnlInput: TPanel;
+    MsgOut: TMemo;
+    Panel1: TPanel;
+    popOut: TPopupMenu;
+    Copy1: TMenuItem;
+    CopyAll1: TMenuItem;
+    Clear1: TMenuItem;
+    Emoticons1: TMenuItem;
+    procedure Emoticons1Click(Sender: TObject);
+    procedure MsgListURLClick(Sender: TObject; url: String);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    procedure SetEmoticon(msn: boolean; imgIndex: integer);
+  end;
+
+var
+  frmBaseChat: TfrmBaseChat;
+
+implementation
+
+{$R *.dfm}
+uses
+    MsgDisplay, ShellAPI, Emoticons, Jabber1;
+
+{---------------------------------------}
+procedure TfrmBaseChat.Emoticons1Click(Sender: TObject);
+var
+    l, t: integer;
+    cp: TPoint;
+begin
+  inherited;
+    // Show the emoticons form
+    GetCaretPos(cp);
+    l := MsgOut.ClientOrigin.x + cp.X;
+
+    if (Self.Docked) then begin
+        t := frmJabber.Top + frmJabber.ClientHeight - 10;
+        frmEmoticons.Left := l + 10;
+        end
+    else begin
+        t := Self.Top + Self.ClientHeight - 10;
+        frmEmoticons.Left := l + 10;
+        end;
+
+    if ((t + frmEmoticons.Height) > Screen.Height) then
+        t := Screen.Height - frmEmoticons.Height;
+
+    frmEmoticons.Top := t;
+    frmEmoticons.ChatWindow := Self;
+    frmEmoticons.Show;
+end;
+
+{---------------------------------------}
+procedure TfrmBaseChat.SetEmoticon(msn: boolean; imgIndex: integer);
+var
+    l, i, m: integer;
+    eo: TEmoticon;
+begin
+    // Setup some Emoticon
+    m := -1;
+
+    if (emoticon_list.Count = 0) then
+        ConfigEmoticons();
+
+    for i := 0 to emoticon_list.Count - 1 do begin
+        eo := TEmoticon(emoticon_list.Objects[i]);
+        if (((msn) and (eo.il = frmJabber.imgMSNEmoticons)) or
+        ((not msn) and (eo.il = frmJabber.imgYahooEmoticons))) then begin
+            // the image lists match
+            if (eo.idx = imgIndex) then begin
+                m := i;
+                break;
+                end;
+            end;
+        end;
+
+    if (m >= 0) then begin
+        l := length(MsgOut.Text);
+        if ((l > 0) and ((MsgOut.Text[l]) <> ' ')) then
+            MsgOut.SelText := ' ';
+        MsgOut.SelText := emoticon_list[m];
+        end;
+end;
+
+
+procedure TfrmBaseChat.MsgListURLClick(Sender: TObject; url: String);
+begin
+    ShellExecute(0, 'open', PChar(url), nil, nil, SW_SHOWNORMAL);
+end;
+
+end.
