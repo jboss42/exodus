dcf9fc2a2b1b09a0a4d496ddff42f392eee55b16
hildjj <hildjj@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2003-01-15 06:29:33 +0000

    Daily build

--- trunk/exodus/version.h
+++ trunk/exodus/version.h
@@ -1,2 +1,2 @@
-#define EXVERSION        0,8,1,6
-#define EXVERSIONSTR     "0.8.1.6\0"
+#define EXVERSION        0,8,1,7
+#define EXVERSIONSTR     "0.8.1.7\0"
--- trunk/exodus/version.nsi
+++ trunk/exodus/version.nsi
@@ -1,1 +1,1 @@
-!define MUI_VERSION "0.8.1.6"
+!define MUI_VERSION "0.8.1.7"
