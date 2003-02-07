7847a91a7972c551e03d019edc50184125b2cdc5
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2003-02-07 14:36:15 +0000

    The netmeeting stuff may actually work

--- exodus/plugins/NetMeeting/ExNetMeeting.dpr
+++ exodus/plugins/NetMeeting/ExNetMeeting.dpr
@@ -3,7 +3,19 @@ library ExNetMeeting;
 uses
   ComServ,
   ExNetMeeting_TLB in 'ExNetMeeting_TLB.pas',
-  NMPlugin in 'NMPlugin.pas' {ExNetmeetingPlugin: CoClass};
+  NMPlugin in 'NMPlugin.pas' {ExNetmeetingPlugin: CoClass},
+  Unicode in '..\..\..\jopl\Unicode.pas',
+  XMLAttrib in '..\..\..\jopl\XMLAttrib.pas',
+  XMLCData in '..\..\..\jopl\XMLCData.pas',
+  XMLConstants in '..\..\..\jopl\XMLConstants.pas',
+  XMLNode in '..\..\..\jopl\XMLNode.pas',
+  XMLTag in '..\..\..\jopl\XMLTag.pas',
+  XMLUtils in '..\..\..\jopl\XMLUtils.pas',
+  LibXmlComps in '..\..\..\jopl\LibXmlComps.pas',
+  LibXmlParser in '..\..\..\jopl\LibXmlParser.pas',
+  SecHash in '..\..\..\jopl\SecHash.pas',
+  NetMeetingLib_TLB in 'NetMeetingLib_TLB.pas',
+  XMLParser in '..\..\..\jopl\XMLParser.pas';
 
 exports
   DllGetClassObject,
