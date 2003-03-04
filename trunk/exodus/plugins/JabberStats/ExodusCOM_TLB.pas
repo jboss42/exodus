85571376755cc72d8f87b4e7580cab78cb50058d
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2003-03-04 14:15:24 +0000

    Fixed up project file

--- trunk/exodus/plugins/JabberStats/ExodusCOM_TLB.pas
+++ trunk/exodus/plugins/JabberStats/ExodusCOM_TLB.pas
@@ -12,7 +12,7 @@ unit ExodusCOM_TLB;
 // ************************************************************************ //
 
 // PASTLWTR : 1.2
-// File generated on 3/4/2003 7:07:39 AM from Type Library described below.
+// File generated on 3/4/2003 7:15:10 AM from Type Library described below.
 
 // ************************************************************************  //
 // Type Lib: D:\src\exodus\runner\Exodus.exe (1)
