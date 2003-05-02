64da368b50a71966448fc114a7eca8cdb471258d
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2003-05-02 13:29:25 +0000

    Check for Winsock2 before starting

--- trunk/exodus/ExodusCOM_TLB.pas
+++ trunk/exodus/ExodusCOM_TLB.pas
@@ -12,10 +12,10 @@ unit ExodusCOM_TLB;
 // ************************************************************************ //
 
 // PASTLWTR : 1.2
-// File generated on 4/27/2003 8:59:33 PM from Type Library described below.
+// File generated on 5/2/2003 7:25:46 AM from Type Library described below.
 
 // ************************************************************************  //
-// Type Lib: D:\Src\exodus\exodus\Exodus.tlb (1)
+// Type Lib: D:\src\exodus\exodus\Exodus.tlb (1)
 // LIBID: {219E0029-5710-4C9B-BE33-4C7F046D7792}
 // LCID: 0
 // Helpfile: 
