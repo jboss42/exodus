058f4c5928e8cfe928e110aad21cf06532154f66
hildjj <hildjj@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2003-09-09 19:07:30 +0000

    daily build

--- exodus/version.h
+++ exodus/version.h
@@ -1,2 +1,2 @@
-#define EXVERSION        0,8,6,9
-#define EXVERSIONSTR     "0.8.6.9\0"
+#define EXVERSION        0,8,6,10
+#define EXVERSIONSTR     "0.8.6.10\0"
--- exodus/version.nsi
+++ exodus/version.nsi
@@ -1,1 +1,1 @@
-!define MUI_VERSION "0.8.6.9"
+!define MUI_VERSION "0.8.6.10"
