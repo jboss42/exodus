8035e8a5463e61cdd188a016aa5361e7535950a4
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2003-11-19 14:17:23 +0000

    .

--- trunk/exodus/plugins/ASpeller/ExodusCOM_TLB.pas
+++ trunk/exodus/plugins/ASpeller/ExodusCOM_TLB.pas
@@ -12,16 +12,16 @@ unit ExodusCOM_TLB;
 // ************************************************************************ //
 
 // PASTLWTR : 1.2
-// File generated on 11/7/2003 5:38:26 PM from Type Library described below.
+// File generated on 11/15/2003 3:54:17 PM from Type Library described below.
 
 // ************************************************************************  //
-// Type Lib: C:\src\exodus\exodus\Exodus.tlb (1)
+// Type Lib: D:\src\exodus\exodus\Exodus.tlb (1)
 // LIBID: {5BABCA07-A359-4B42-8C03-C5B329E79E31}
 // LCID: 0
 // Helpfile: 
 // HelpString: Exodus COM Plugin interfaces
 // DepndLst: 
-//   (1) v2.0 stdole, (C:\Windows\System32\stdole2.tlb)
+//   (1) v2.0 stdole, (C:\WINDOWS\System32\stdole2.tlb)
 // ************************************************************************ //
 {$TYPEDADDRESS OFF} // Unit must be compiled without type-checked pointers. 
 {$WARN SYMBOL_PLATFORM OFF}
