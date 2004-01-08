8be25b81f504ee7c2c44f5ce46e8bde8519a3e7a
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2004-01-08 04:24:00 +0000

    minor cleanups

--- trunk/exodus/jud.pas
+++ trunk/exodus/jud.pas
@@ -145,11 +145,6 @@ begin
     // create a new room
     f := TfrmJUD.Create(Application);
 
-    {
-    if MainSession.Prefs.getBool('expanded') then
-        f.DockForm;
-    }
-
     // populate the drop down box based on our entity cache
     f.cboJID.Items.Clear();
     jEntityCache.getByFeature(FEAT_SEARCH, f.cboJID.Items);
