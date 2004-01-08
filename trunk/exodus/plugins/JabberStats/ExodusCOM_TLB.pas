52eee8fdf39055530b29bb897919b94f6dad3fed
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2004-01-08 06:13:24 +0000

    .

--- trunk/exodus/plugins/JabberStats/ExodusCOM_TLB.pas
+++ trunk/exodus/plugins/JabberStats/ExodusCOM_TLB.pas
@@ -12,7 +12,7 @@ unit ExodusCOM_TLB;
 // ************************************************************************ //
 
 // PASTLWTR : 1.2
-// File generated on 12/31/2003 10:25:41 AM from Type Library described below.
+// File generated on 1/7/2004 9:25:04 PM from Type Library described below.
 
 // ************************************************************************  //
 // Type Lib: D:\src\exodus\exodus\Exodus.tlb (1)
