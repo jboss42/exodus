c55e0422c48cb2a4168c5f2897b1edf777e15715
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2004-01-08 05:31:40 +0000

    Bug #3153 - Allow renaming of groups when only change is case. Also gets us case insensitive searching for group names during roster management ops.

--- trunk/exodus/ExodusCOM_TLB.pas
+++ trunk/exodus/ExodusCOM_TLB.pas
@@ -12,7 +12,7 @@ unit ExodusCOM_TLB;
 // ************************************************************************ //
 
 // PASTLWTR : 1.2
-// File generated on 1/7/2004 9:23:41 PM from Type Library described below.
+// File generated on 1/7/2004 9:25:04 PM from Type Library described below.
 
 // ************************************************************************  //
 // Type Lib: D:\src\exodus\exodus\Exodus.tlb (1)
