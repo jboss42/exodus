fa33f09485093d0a7c47ca33ff7853a790bfbd90
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2004-04-01 16:17:15 +0000

    daily build

--- trunk/exodus/version.h
+++ trunk/exodus/version.h
@@ -1,2 +1,2 @@
-#define EXVERSION        0,9,0,7
-#define EXVERSIONSTR     "0.9.0.7\0"
+#define EXVERSION        0,9,0,8
+#define EXVERSIONSTR     "0.9.0.8\0"
--- trunk/exodus/version.nsi
+++ trunk/exodus/version.nsi
@@ -1,1 +1,1 @@
-!define EXODUS_VERSION "0.9.0.7"
+!define EXODUS_VERSION "0.9.0.8"
