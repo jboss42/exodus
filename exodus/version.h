7410ebd28b37115fd4b6d51526375104d91a3c6f
pgmillard <pgmillard@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2004-06-16 22:46:16 +0000

    daily build

--- exodus/version.h
+++ exodus/version.h
@@ -1,2 +1,2 @@
-#define EXVERSION        0,9,0,19
-#define EXVERSIONSTR     "0.9.0.19\0"
+#define EXVERSION        0,9,0,20
+#define EXVERSIONSTR     "0.9.0.20\0"
--- exodus/version.nsi
+++ exodus/version.nsi
@@ -1,1 +1,1 @@
-!define EXODUS_VERSION "0.9.0.19"
+!define EXODUS_VERSION "0.9.0.20"
