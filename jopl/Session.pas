b42a295f0c1649761b29b708ac5bf28bd6d7b9e1
hildjj <hildjj@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2006-06-27 23:05:13 +0000

    Fixed up whitespace after reviewing snickers' change for auth agent not being created each connect.

--- jopl/Session.pas
+++ jopl/Session.pas
@@ -1197,22 +1197,19 @@ begin
     assert(_profile <> nil); //should not try to set authagent until profile is set
     assert(_stream = nil); //should not try to change authagent oncve connected
 
-    //if (not assigned(_auth_agent)) then begin
-        // Create the AuthAgent
-        if (profile.SSL_Cert <> '')  then
-            auth := CreateJabberAuth('EXTERNAL', Self)
-        else if (_profile.KerbAuth) then
-            auth := CreateJabberAuth('GSSAPI', Self)
-        else
-            auth := CreateJabberAuth('XMPP', Self);
+    // Create the AuthAgent
+    if (profile.SSL_Cert <> '')  then
+        auth := CreateJabberAuth('EXTERNAL', Self)
+    else if (_profile.KerbAuth) then
+        auth := CreateJabberAuth('GSSAPI', Self)
+    else
+        auth := CreateJabberAuth('XMPP', Self);
 
-        if (auth = nil) then
-            raise Exception.Create('No appropriate Auth Agent found.');
+    if (auth = nil) then
+        raise Exception.Create('No appropriate Auth Agent found.');
 
-        // set this auth agent as our current one
-        setAuthAgent(auth);
-    //end;
-    
+    // set this auth agent as our current one
+    setAuthAgent(auth);
 end;
 
 {---------------------------------------}
