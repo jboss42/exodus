7989572982844238f82ff3aecd6b56a7e9ad0b08
jfuhrman <jfuhrman@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2006-10-17 16:16:24 +0000

    bring to front notification work

--- branches/Hermes_PR2/exodus/StateForm.pas
+++ branches/Hermes_PR2/exodus/StateForm.pas
@@ -230,6 +230,8 @@ type
 
         The default implementation is to show the window in its last floating
         position. Override this method to change (ie dock instead of float)
+        
+        @param bringtofront bring the window to the top of the zorder
     }
     procedure ShowDefault(bringtofront:boolean=true);virtual;
 
@@ -611,7 +613,6 @@ end;
 {---------------------------------------}
 procedure TfrmState.WMActivate(var msg: TMessage);
 begin
-outputdebugmsg('TfrmState.WMActivate got WMActivate: ' + IntToStr(Msg.WParamLo));
     if (not _skipWindowPosHandling and (Msg.WParamLo <> WA_INACTIVE)) then begin
         // we are getting activated
         _skipWindowPosHandling := true;
@@ -713,18 +714,20 @@ begin
             ShowWindow(Handle, SW_SHOWMINNOACTIVE)
         else if (_windowState = wsMaximized) then
             ShowWindow(Handle, SW_MAXIMIZE)
-        else if(bringtofront) then
-            ShowWindow(Handle, SW_SHOWNORMAL)
+        else if(bringtofront) then begin
+            ShowWindow(Handle, SW_SHOWNORMAL);
+            Self.BringToFront;
+        end
         else
-            ShowWindow(Handle, SW_SHOWNOACTIVATE);
+            SetWindowPos(Self.Handle, HWND_BOTTOM, 0,0,0,0, SWP_NOSIZE + SWP_NOMOVE + SWP_NOACTIVATE + SWP_SHOWWINDOW);
+        Self.Visible := true;   
     end
     else if (frmExodus.isMinimized() and not bringtofront) then
         ShowWindow(Handle, SW_SHOWMINNOACTIVE)
-    else if(bringtofront) then
-        ShowWindow(Handle, SW_SHOWNORMAL)
+    else if(not bringtofront) then
+        ShowWindow(Handle, SW_SHOWNOACTIVATE)
     else
-        ShowWindow(Handle, SW_SHOWNOACTIVATE);
-    Self.Show();        
+        ShowWindow(Handle, SW_SHOWNORMAL)
 end;
 
 procedure TfrmState.gotActivate();
@@ -787,7 +790,7 @@ begin
     normalizePos();
     //setwiondowpos sets the undocked dimensions of window.
     _skipWindowPosHandling := true;
-    SetWindowPos(Self.Handle, 0, _pos.Left, _pos.Top, _pos.Width, _pos.Height, SWP_NOOWNERZORDER);
+    SetWindowPos(Self.Handle, HWND_BOTTOM, _pos.Left, _pos.Top, _pos.Width, _pos.Height, SWP_NOACTIVATE or SWP_NOOWNERZORDER);
     _skipWindowPosHandling := false;
     //minimized, maximized or restored
     _windowState := sToWindowState(windowState.GetAttribute('ws'));
@@ -889,13 +892,8 @@ begin
      end;
 
      if ((notifyEvents and notify_front) > 0) then begin
-        if (not Self.Visible or (Self.WindowState = wsMinimized)) then begin
-            Self.Visible := true;
-            Self.WindowState := wsNormal;
-            Self.Show();
-        end;
         ShowWindow(Self.Handle, SW_SHOWNORMAL);
-        ForceForegroundWindow(Self.Handle);
+        Self.Show();
      end;
 end;
 
