48f6f6479e8ac35d2ef46645a29ba4d837f99391
jfuhrman <jfuhrman@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2006-12-05 23:36:35 +0000

    removed relative paths to Exodus files from .dpr

--- exodus/plugins/SIGQuickChat/SIGQuickChat.dpr
+++ exodus/plugins/SIGQuickChat/SIGQuickChat.dpr
@@ -5,27 +5,12 @@ library SIGQuickChat;
 
 uses
   ComServ,
-  Unicode in '..\..\..\jopl\Unicode.pas',
-  LibXmlParser in '..\..\..\jopl\LibXmlParser.pas',
-  XMLTag in '..\..\..\jopl\XMLTag.pas',
-  XMLNode in '..\..\..\jopl\XMLNode.pas',
-  XMLAttrib in '..\..\..\jopl\XMLAttrib.pas',
-  XMLUtils in '..\..\..\jopl\XMLUtils.pas',
-  XMLCData in '..\..\..\jopl\XMLCData.pas',
-  SecHash in '..\..\..\jopl\SecHash.pas',
-  XMLParser in '..\..\..\jopl\XMLParser.pas',
   SIGQuickChat_TLB in 'SIGQuickChat_TLB.pas',
   ActiveFormImpl in 'ActiveFormImpl.pas' {ActiveFormX: TActiveForm} {ActiveFormX: CoClass},
   SIGQuickChatPlugin in 'SIGQuickChatPlugin.pas',
   Controller in 'Controller.pas',
-  XMLConstants in '..\..\..\jopl\XMLConstants.pas',
   ComboBoxExSIG in 'Packages\ComboBoxExSIG.pas',
-  Options in 'Options.pas' {Form1},
-  JclStrings in '..\..\components\jcl\source\common\JclStrings.pas',
-  JclWideStrings in '..\..\components\jcl\source\common\JclWideStrings.pas',
-  JclBase in '..\..\components\jcl\source\common\JclBase.pas',
-  JclResources in '..\..\components\jcl\source\common\JclResources.pas',
-  JclLogic in '..\..\components\jcl\source\common\JclLogic.pas';
+  Options in 'Options.pas';{Form1}
 
 {$E dll}
 exports
