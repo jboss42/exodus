75a2901b787214953f48d39cb49f2590c2dcffa9
yelena <yelena@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2006-12-08 23:04:53 +0000

    Added feature from SIG to implement required plug-ins.

--- exodus/plugins/Test/TestPlugin_TLB.pas
+++ exodus/plugins/Test/TestPlugin_TLB.pas
@@ -12,39 +12,26 @@ unit TestPlugin_TLB;
 // ************************************************************************ //
 
 // PASTLWTR : 1.2
-// File generated on 8/24/2006 1:42:15 PM from Type Library described below.
+// File generated on 11/17/2006 1:47:54 PM from Type Library described below.
 
 // ************************************************************************  //
-// Type Lib: TestPlugin.tlb (1)
+// Type Lib: C:\source\exodus\exodus\plugins\Test\TestPlugin.tlb (1)
 // LIBID: {78FCE930-6D97-4E80-A634-59897D6E8BB2}
 // LCID: 0
 // Helpfile: 
 // HelpString: TestPlugin Library
 // DepndLst: 
-//   (1) v1.0 Exodus, (C:\Projects\Devel\Clients\Hermes\bin\Exodus.exe)
+//   (1) v1.0 Exodus, (C:\Program Files\Jabber Inc\Hermes\Hermes.exe)
 //   (2) v2.0 stdole, (C:\WINDOWS\system32\stdole2.tlb)
-// Errors:
-//   Error creating palette bitmap of (TTesterPlugin) : No Server registered for this CoClass
 // ************************************************************************ //
-// *************************************************************************//
-// NOTE:                                                                      
-// Items guarded by $IFDEF_LIVE_SERVER_AT_DESIGN_TIME are used by properties  
-// which return objects that may need to be explicitly created via a function 
-// call prior to any access via the property. These items have been disabled  
-// in order to prevent accidental use from within the object inspector. You   
-// may enable them by defining LIVE_SERVER_AT_DESIGN_TIME or by selectively   
-// removing them from the $IFDEF blocks. However, such items must still be    
-// programmatically created via a method of the appropriate CoClass before    
-// they can be used.                                                          
 {$TYPEDADDRESS OFF} // Unit must be compiled without type-checked pointers. 
 {$WARN SYMBOL_PLATFORM OFF}
 {$WRITEABLECONST ON}
 {$VARPROPSETTER ON}
 interface
 
-uses Windows, ActiveX, Classes, Exodus_TLB, Graphics, OleServer, StdVCL, Variants;
+uses Windows, ActiveX, Classes, Exodus_TLB, Graphics, StdVCL, Variants;
   
-
 
 // *********************************************************************//
 // GUIDS declared in the TypeLibrary. Following prefixes are used:        
@@ -62,6 +49,8 @@ const
 
   IID_ITesterPlugin: TGUID = '{AE5CDE9B-CFD3-4BE9-9855-C1A1BD52A089}';
   CLASS_TesterPlugin: TGUID = '{DE6D1148-AC93-412F-AF4B-F26C24136D2C}';
+  IID_IChatPlugin: TGUID = '{BFA20B37-18C9-4B90-9BD3-B59F856AC625}';
+  CLASS_ChatPlugin: TGUID = '{B8EAE0B0-F374-4A7D-9285-52A5389DB3B4}';
 type
 
 // *********************************************************************//
@@ -69,12 +58,15 @@ type
 // *********************************************************************//
   ITesterPlugin = interface;
   ITesterPluginDisp = dispinterface;
+  IChatPlugin = interface;
+  IChatPluginDisp = dispinterface;
 
 // *********************************************************************//
 // Declaration of CoClasses defined in Type Library                       
 // (NOTE: Here we map each CoClass to its Default Interface)              
 // *********************************************************************//
   TesterPlugin = IExodusPlugin;
+  ChatPlugin = IChatPlugin;
 
 
 // *********************************************************************//
@@ -105,6 +97,32 @@ type
   end;
 
 // *********************************************************************//
+// Interface: IChatPlugin
+// Flags:     (4416) Dual OleAutomation Dispatchable
+// GUID:      {BFA20B37-18C9-4B90-9BD3-B59F856AC625}
+// *********************************************************************//
+  IChatPlugin = interface(IExodusChatPlugin)
+    ['{BFA20B37-18C9-4B90-9BD3-B59F856AC625}']
+  end;
+
+// *********************************************************************//
+// DispIntf:  IChatPluginDisp
+// Flags:     (4416) Dual OleAutomation Dispatchable
+// GUID:      {BFA20B37-18C9-4B90-9BD3-B59F856AC625}
+// *********************************************************************//
+  IChatPluginDisp = dispinterface
+    ['{BFA20B37-18C9-4B90-9BD3-B59F856AC625}']
+    function OnBeforeMessage(var Body: WideString): WordBool; dispid 1;
+    function OnAfterMessage(var Body: WideString): WideString; dispid 2;
+    procedure OnClose; dispid 6;
+    procedure OnNewWindow(hwnd: Integer); dispid 202;
+    function OnBeforeRecvMessage(const Body: WideString; const XML: WideString): WordBool; dispid 203;
+    procedure OnAfterRecvMessage(var Body: WideString); dispid 204;
+    function OnKeyUp(key: Integer; shiftState: Integer): WordBool; dispid 301;
+    function OnKeyDown(key: Integer; shiftState: Integer): WordBool; dispid 201;
+  end;
+
+// *********************************************************************//
 // The Class CoTesterPlugin provides a Create and CreateRemote method to          
 // create instances of the default interface IExodusPlugin exposed by              
 // the CoClass TesterPlugin. The functions are intended to be used by             
@@ -116,77 +134,17 @@ type
     class function CreateRemote(const MachineName: string): IExodusPlugin;
   end;
 
-
 // *********************************************************************//
-// OLE Server Proxy class declaration
-// Server Object    : TTesterPlugin
-// Help String      : TesterPlugin Object
-// Default Interface: IExodusPlugin
-// Def. Intf. DISP? : No
-// Event   Interface: 
-// TypeFlags        : (2) CanCreate
+// The Class CoChatPlugin provides a Create and CreateRemote method to          
+// create instances of the default interface IChatPlugin exposed by              
+// the CoClass ChatPlugin. The functions are intended to be used by             
+// clients wishing to automate the CoClass objects exposed by the         
+// server of this typelibrary.                                            
 // *********************************************************************//
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-  TTesterPluginProperties= class;
-{$ENDIF}
-  TTesterPlugin = class(TOleServer)
-  private
-    FIntf: IExodusPlugin;
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-    FProps: TTesterPluginProperties;
-    function GetServerProperties: TTesterPluginProperties;
-{$ENDIF}
-    function GetDefaultInterface: IExodusPlugin;
-  protected
-    procedure InitServerData; override;
-  public
-    constructor Create(AOwner: TComponent); override;
-    destructor  Destroy; override;
-    procedure Connect; override;
-    procedure ConnectTo(svrIntf: IExodusPlugin);
-    procedure Disconnect; override;
-    procedure Startup(const exodusController: IExodusController);
-    procedure Shutdown;
-    procedure Process(const xpath: WideString; const event: WideString; const XML: WideString);
-    procedure NewChat(const JID: WideString; const chat: IExodusChat);
-    procedure NewRoom(const JID: WideString; const room: IExodusChat);
-    function NewIM(const JID: WideString; var Body: WideString; var Subject: WideString; 
-                   const xTags: WideString): WideString;
-    procedure Configure;
-    procedure NewOutgoingIM(const JID: WideString; const instantMsg: IExodusChat);
-    property DefaultInterface: IExodusPlugin read GetDefaultInterface;
-  published
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-    property Server: TTesterPluginProperties read GetServerProperties;
-{$ENDIF}
+  CoChatPlugin = class
+    class function Create: IChatPlugin;
+    class function CreateRemote(const MachineName: string): IChatPlugin;
   end;
-
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-// *********************************************************************//
-// OLE Server Properties Proxy Class
-// Server Object    : TTesterPlugin
-// (This object is used by the IDE's Property Inspector to allow editing
-//  of the properties of this server)
-// *********************************************************************//
- TTesterPluginProperties = class(TPersistent)
-  private
-    FServer:    TTesterPlugin;
-    function    GetDefaultInterface: IExodusPlugin;
-    constructor Create(AServer: TTesterPlugin);
-  protected
-  public
-    property DefaultInterface: IExodusPlugin read GetDefaultInterface;
-  published
-  end;
-{$ENDIF}
-
-
-procedure Register;
-
-resourcestring
-  dtlServerPage = 'Servers';
-
-  dtlOcxPage = 'ActiveX';
 
 implementation
 
@@ -202,133 +160,14 @@ begin
   Result := CreateRemoteComObject(MachineName, CLASS_TesterPlugin) as IExodusPlugin;
 end;
 
-procedure TTesterPlugin.InitServerData;
-const
-  CServerData: TServerData = (
-    ClassID:   '{DE6D1148-AC93-412F-AF4B-F26C24136D2C}';
-    IntfIID:   '{6D6CCD11-2FAA-4CCB-92CA-CAB14A3BE234}';
-    EventIID:  '';
-    LicenseKey: nil;
-    Version: 500);
+class function CoChatPlugin.Create: IChatPlugin;
 begin
-  ServerData := @CServerData;
+  Result := CreateComObject(CLASS_ChatPlugin) as IChatPlugin;
 end;
 
-procedure TTesterPlugin.Connect;
-var
-  punk: IUnknown;
+class function CoChatPlugin.CreateRemote(const MachineName: string): IChatPlugin;
 begin
-  if FIntf = nil then
-  begin
-    punk := GetServer;
-    Fintf:= punk as IExodusPlugin;
-  end;
-end;
-
-procedure TTesterPlugin.ConnectTo(svrIntf: IExodusPlugin);
-begin
-  Disconnect;
-  FIntf := svrIntf;
-end;
-
-procedure TTesterPlugin.DisConnect;
-begin
-  if Fintf <> nil then
-  begin
-    FIntf := nil;
-  end;
-end;
-
-function TTesterPlugin.GetDefaultInterface: IExodusPlugin;
-begin
-  if FIntf = nil then
-    Connect;
-  Assert(FIntf <> nil, 'DefaultInterface is NULL. Component is not connected to Server. You must call ''Connect'' or ''ConnectTo'' before this operation');
-  Result := FIntf;
-end;
-
-constructor TTesterPlugin.Create(AOwner: TComponent);
-begin
-  inherited Create(AOwner);
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-  FProps := TTesterPluginProperties.Create(Self);
-{$ENDIF}
-end;
-
-destructor TTesterPlugin.Destroy;
-begin
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-  FProps.Free;
-{$ENDIF}
-  inherited Destroy;
-end;
-
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-function TTesterPlugin.GetServerProperties: TTesterPluginProperties;
-begin
-  Result := FProps;
-end;
-{$ENDIF}
-
-procedure TTesterPlugin.Startup(const exodusController: IExodusController);
-begin
-  DefaultInterface.Startup(exodusController);
-end;
-
-procedure TTesterPlugin.Shutdown;
-begin
-  DefaultInterface.Shutdown;
-end;
-
-procedure TTesterPlugin.Process(const xpath: WideString; const event: WideString; 
-                                const XML: WideString);
-begin
-  DefaultInterface.Process(xpath, event, XML);
-end;
-
-procedure TTesterPlugin.NewChat(const JID: WideString; const chat: IExodusChat);
-begin
-  DefaultInterface.NewChat(JID, chat);
-end;
-
-procedure TTesterPlugin.NewRoom(const JID: WideString; const room: IExodusChat);
-begin
-  DefaultInterface.NewRoom(JID, room);
-end;
-
-function TTesterPlugin.NewIM(const JID: WideString; var Body: WideString; var Subject: WideString; 
-                             const xTags: WideString): WideString;
-begin
-  Result := DefaultInterface.NewIM(JID, Body, Subject, xTags);
-end;
-
-procedure TTesterPlugin.Configure;
-begin
-  DefaultInterface.Configure;
-end;
-
-procedure TTesterPlugin.NewOutgoingIM(const JID: WideString; const instantMsg: IExodusChat);
-begin
-  DefaultInterface.NewOutgoingIM(JID, instantMsg);
-end;
-
-{$IFDEF LIVE_SERVER_AT_DESIGN_TIME}
-constructor TTesterPluginProperties.Create(AServer: TTesterPlugin);
-begin
-  inherited Create;
-  FServer := AServer;
-end;
-
-function TTesterPluginProperties.GetDefaultInterface: IExodusPlugin;
-begin
-  Result := FServer.DefaultInterface;
-end;
-
-{$ENDIF}
-
-procedure Register;
-begin
-  RegisterComponents(dtlServerPage, [TTesterPlugin]);
+  Result := CreateRemoteComObject(MachineName, CLASS_ChatPlugin) as IChatPlugin;
 end;
 
 end.
