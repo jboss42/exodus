43993bdd509ae68ec6552aa00d048ad1defbfcfa
dabbink <dabbink@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2007-04-12 21:05:51 +0000

    made a copy

---
+++ tags/hermes_QA/exodus/PathSelector.pas
@@ -0,0 +1,56 @@
+unit PathSelector;
+
+interface
+
+{$WARN UNIT_PLATFORM OFF}
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, buttonFrame, ComCtrls, ShellCtrls, TntStdCtrls;
+
+type
+  TfrmPathSelector = class(TForm)
+    frameButtons1: TframeButtons;
+    Label1: TTntLabel;
+    Folders: TShellTreeView;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPathSelector: TfrmPathSelector;
+
+function browsePath(var SelectedPath: string): boolean;
+
+implementation
+{$R *.dfm}
+uses
+    GnuGetText;
+
+function browsePath(var SelectedPath: string): boolean;
+var
+    f: TfrmPathSelector;
+begin
+    //
+    Result := false;
+    f := TfrmPathSelector.Create(nil);
+    if (SelectedPath <> '') then
+        f.Folders.Path := SelectedPath;
+
+    if (f.ShowModal = mrOK) then begin
+        SelectedPath := f.Folders.Path;
+        Result := true;
+    end;
+    f.Free();
+end;
+
+
+procedure TfrmPathSelector.FormCreate(Sender: TObject);
+begin
+    TranslateComponent(Self);
+end;
+
+end.
---
+++ tags/hermes_QA/exodus/prefs/PrefPanel.dfm
@@ -0,0 +1,31 @@
+object frmPrefPanel: TfrmPrefPanel
+  Left = 408
+  Top = 423
+  BorderStyle = bsNone
+  BorderWidth = 6
+  Caption = 'frmPrefPanel'
+  ClientHeight = 301
+  ClientWidth = 270
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object pnlHeader: TTntPanel
+    Left = 0
+    Top = 0
+    Width = 270
+    Height = 26
+    Align = alTop
+    BevelOuter = bvLowered
+    Caption = 'pnlHeader'
+    Color = clHighlight
+    ParentBackground = False
+    TabOrder = 0
+  end
+end
