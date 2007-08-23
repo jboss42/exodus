63767605b43a02b4b706b07b7ac4dfe69f6c3b85
yelena <yelena@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2007-08-23 21:24:56 +0000

    TT 61276. Fixed the code to not add "Transfer Files" notification option if it is branded off. Updated all references to const NUM_NOTIFIES to use member variable to reflect smaller size of notification array.

--- branches/JMIM_5.2.1/exodus/prefs/PrefNotify.pas
+++ branches/JMIM_5.2.1/exodus/prefs/PrefNotify.pas
@@ -50,7 +50,7 @@ type
     _notify: array of integer;
     _no_notify_update: boolean;
     _loading: boolean;
-
+    _numNotifies: Integer;
   public
     { Public declarations }
     procedure LoadPrefs(); override;
@@ -91,6 +91,7 @@ var
 begin
 
     _loading := true;
+    _numNotifies := NUM_NOTIFIES;
 
     chkNotify.Items.Clear();
     chkNotify.Items.Add(_(sSoundOnline));
@@ -103,10 +104,15 @@ begin
     chkNotify.Items.Add(_(sSoundChatactivity));
     chkNotify.Items.Add(_(sSoundPriorityChatactivity));
     chkNotify.Items.Add(_(sSoundRoomactivity));
-    chkNotify.Items.Add(_(sSoundPriorityRoomactivity));    
-    chkNotify.Items.Add(_(sSoundOOB));
+    chkNotify.Items.Add(_(sSoundPriorityRoomactivity));
     chkNotify.Items.Add(_(sSoundAutoResponse));
-    SetLength(_notify, NUM_NOTIFIES);
+
+    if (MainSession.Prefs.getBool('brand_ft')) then
+       chkNotify.Items.Add(_(sSoundOOB))
+    else
+       _numNotifies := NUM_NOTIFIES - 1;
+
+    SetLength(_notify, _numNotifies);
 
     inherited;
 
@@ -123,8 +129,9 @@ begin
         _notify[8]  := getInt('notify_priority_chatactivity');
         _notify[9]  := getInt('notify_roomactivity');
         _notify[10] := getInt('notify_priority_roomactivity');
-        _notify[11] := getInt('notify_oob');
-        _notify[12] := getInt('notify_autoresponse');
+        _notify[11] := getInt('notify_autoresponse');
+        if (MainSession.Prefs.getBool('brand_ft')) then
+             _notify[12] := getInt('notify_oob');
 
         optNotify.Enabled;
         chkToast.Checked := false;
@@ -132,7 +139,7 @@ begin
         chkTrayNotify.Checked := false;
         chkFront.Checked := false;
 
-        for i := 0 to NUM_NOTIFIES - 1 do
+        for i := 0 to _numNotifies - 1 do
             chkNotify.Checked[i] := (_notify[i] > 0);
 
     end;
@@ -165,8 +172,9 @@ begin
         setInt('notify_priority_chatactivity', _notify[8]);
         setInt('notify_roomactivity', _notify[9]);
         setInt('notify_priority_roomactivity', _notify[10]);
-        setInt('notify_oob', _notify[11]);
-        setInt('notify_autoresponse', _notify[12]);
+        setInt('notify_autoresponse', _notify[11]);
+        if (MainSession.Prefs.getBool('brand_ft')) then
+             setInt('notify_oob', _notify[12]);
     end;
 end;
 
@@ -194,7 +202,10 @@ end;
 procedure TfrmPrefNotify.FormCreate(Sender: TObject);
 begin
   inherited;
-    SetLength(_notify, NUM_NOTIFIES);
+    _numNotifies := NUM_NOTIFIES;
+    if (not MainSession.Prefs.getBool('brand_ft')) then
+        _numNotifies :=  NUM_NOTIFIES - 1;
+    SetLength(_notify, _numNotifies);
     AssignUnicodeURL(lblConfigSounds.Font, 8);
 end;
 
@@ -241,7 +252,7 @@ begin
     i := chkNotify.ItemIndex;
 
     if (i < 0) then exit;
-    if (i > NUM_NOTIFIES) then exit;
+    if (i > _numNotifies) then exit;
 
     _notify[i] := 0;
     if (chkToast.Checked) then _notify[i] := _notify[i] + notify_toast;
