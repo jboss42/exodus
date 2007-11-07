ed9929cc477eb5404110f170da67c1ef006366ba
y1.rabinovich <y1.rabinovich@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2007-11-07 21:46:44 +0000

    Added a folder remotely

---
+++ trunk/exodus/components/base/ExForm.pas
@@ -0,0 +1,24 @@
+unit ExForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, TntForms;
+
+type
+  TExForm = class(TTntForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  BaseForm: TExForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ trunk/exodus/components/base/ExFrame.pas
@@ -0,0 +1,24 @@
+unit ExFrame;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, TntForms;
+
+type
+  TExFrame = class(TTntFrame)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  BaseFrame: TExFrame;
+
+implementation
+
+{$R *.dfm}
+
+end.
