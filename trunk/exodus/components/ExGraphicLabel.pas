e288fa94b345f0fb866b0bbd69bff3c104d0be77
linuxwolf <linuxwolf@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-01-09 23:55:30 +0000

    start of focus handling

--- trunk/exodus/components/ExGraphicLabel.pas
+++ trunk/exodus/components/ExGraphicLabel.pas
@@ -10,6 +10,7 @@ type
   TExGraphicLabel = class(TCustomControl)
   private
     { Private declarations }
+    _focused: boolean;
     _selected: boolean;
     _caption: Widestring;
     _border: TBorderWidth;
@@ -17,6 +18,7 @@ type
 
     _target: TObject;
 
+    procedure SetFocued(f: boolean);
     procedure SetCaption(txt: WideString);
     procedure SetSelected(sel: boolean);
     procedure SetBorder(border: TBorderWidth);
@@ -53,40 +55,46 @@ implementation
 
 uses Types, Windows, TnTWindows;
 
-var
-  gInDesignMode: boolean;
-
 procedure Register;
 begin
-  gInDesignMode := true;
-  RegisterComponents('Win32', [TExGraphicLabel]);
+    RegisterComponents('Win32', [TExGraphicLabel]);
 end;
 
 constructor TExGraphicLabel.Create(AOwner: TComponent);
 begin
-  inherited Create(AOwner);
+    _focused := false;
+    
+    inherited Create(AOwner);
 end;
 
 procedure TExGraphicLabel.SetCaption(txt: WideString);
 begin
-  if txt <> _caption then begin
-    _caption := txt;
-    Invalidate;
-  end;
+    if txt <> _caption then begin
+        _caption := txt;
+        Invalidate;
+    end;
+end;
+procedure TExGraphicLabel.SetFocued(f: Boolean);
+begin
+    if _focused <> f then begin
+        _focused := f;
+        Invalidate;
+    end;
+
 end;
 procedure TExGraphicLabel.SetSelected(sel: Boolean);
 begin
-  if _selected <> sel then begin
-    _selected := sel;
-    Invalidate;
-  end;
+    if _selected <> sel then begin
+        _selected := sel;
+        Invalidate;
+    end;
 end;
 procedure TExGraphicLabel.SetBorder(border: TBorderWidth);
 begin
-  if _border <> border then begin
-    _border := border;
-    Invalidate;
-  end;
+    if _border <> border then begin
+        _border := border;
+        Invalidate;
+    end;
 end;
 procedure TExGraphicLabel.SetKey(key: string);
 begin
@@ -98,50 +106,55 @@ end;
 
 procedure TExGraphicLabel.Paint;
 var
-  OutRect, InRect: TRect;
-  Flags: Longint;
-  Text: Widestring;
-  Png: TPNGObject;
-  Color: TColor;
-  Key: String;
+    OutRect, InRect, fRect: TRect;
+    Flags: Longint;
+    Text: Widestring;
+    Png: TPNGObject;
+    Color: TColor;
+    Key: String;
 
 begin
-  if Selected then begin
-    color := clWhite;
-    key := Self.Key + 'Selected';
-  end
-  else begin
-    color := clBlack;
-    key := Self.Key + 'Enabled';
-  end;
-
-  Text := Caption;
-  InRect := ClientRect;
-  OutRect := InRect;
-  InflateRect(InRect, -BorderWidth, -BorderWidth);
-
-  with Canvas do begin
-    //paint backround image
-    if (not gInDesignMode) and (Self.Key <> '') then begin
-      Png := TPngObject.Create();
-      png.LoadFromResourceName(HInstance, key);
-      png.Draw(Canvas, OutRect);
-      png.Free;
+    if Selected then begin
+        color := clWhite;
+        key := Self.Key + 'Selected';
+    end
+    else begin
+        color := clBlack;
+        key := Self.Key + 'Enabled';
     end;
 
-    if Text <> '' then begin
-      Brush.Style := bsClear;
-      Font.Color := Color;
-      Flags := DT_EXPANDTABS or DT_SINGLELINE or DT_BOTTOM or DT_CENTER;
-      Flags := DrawTextBiDiModeFlags(Flags);
-      Tnt_DrawTextW(Handle, PWideChar(Caption), Length(Caption), InRect, Flags);
-    end;
+    Text := Caption;
+    InRect := ClientRect;
+    OutRect := InRect;
+    InflateRect(InRect, -BorderWidth, -BorderWidth);
+    fRect := InRect;
+    InflateRect(fRect, -2, -2);
+    
+    with Canvas do begin
+        //paint backround image
+        if not (csDesigning in Self.ComponentState) and (Self.Key <> '') then begin
+            Png := TPngObject.Create();
+            png.LoadFromResourceName(HInstance, key);
+            png.Draw(Canvas, OutRect);
+            png.Free;
+        end;
 
-    if gInDesignMode then begin
-      Brush.Style := bsSolid;
-      Brush.Color := clBlack;
-      Canvas.FrameRect(OutRect);
-    end;
+        if Text <> '' then begin
+            Brush.Style := bsClear;
+            Font.Color := Color;
+            Flags := DT_EXPANDTABS or DT_SINGLELINE or DT_BOTTOM or DT_CENTER;
+            Flags := DrawTextBiDiModeFlags(Flags);
+            Tnt_DrawTextW(Handle, PWideChar(Caption), Length(Caption), InRect, Flags);
+        end;
+
+        if _focused then begin
+        end;
+
+        if (csDesigning in Self.ComponentState) then begin
+            Brush.Style := bsSolid;
+            Brush.Color := clBlack;
+            Canvas.FrameRect(inRect);
+        end;
   end;
 end;
 
