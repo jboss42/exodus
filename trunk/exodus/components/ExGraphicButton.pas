298ce34998afab87385f09ccd802119e2edccfa4
linuxwolf <linuxwolf@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-01-10 22:08:12 +0000

    making PNG-based buttons (not labels)

--- trunk/exodus/components/ExGraphicButton.pas
+++ trunk/exodus/components/ExGraphicButton.pas
@@ -1,4 +1,4 @@
-unit ExGraphicLabel;
+unit ExGraphicButton;
 
 interface
 
@@ -7,32 +7,38 @@ uses
   Graphics, pngimage;
 
 type
-  TExGraphicLabel = class(TCustomControl)
+  TExGraphicButton = class(TCustomControl)
   private
     { Private declarations }
     _focused: boolean;
     _selected: boolean;
     _caption: Widestring;
     _border: TBorderWidth;
-    _key: String;
+    _imgEnabled: TPNGObject;
+    _imgSelected: TPNGObject;
 
     _target: TObject;
 
-    procedure SetFocued(f: boolean);
     procedure SetCaption(txt: WideString);
     procedure SetSelected(sel: boolean);
     procedure SetBorder(border: TBorderWidth);
-    procedure SetKey(key: String);
+    procedure SetImageEnabled(img: TPNGObject);
+    procedure SetImageSelected(img: TPNGObject);
 
   protected
     { Protected declarations }
     property AutoSize default false;
 
     procedure Paint(); override;
+    procedure DoEnter(); override;
+    procedure DoExit(); override;
 
   public
     { Public declarations }
     constructor Create(AOwner : TComponent); override;
+    destructor Destroy(); override;
+
+    function CanFocus(): Boolean; override;
 
   published
     { Published declarations }
@@ -42,7 +48,9 @@ type
     property Caption: Widestring read _caption write SetCaption;
     property Enabled default true;
     property Selected: boolean read _selected write SetSelected default false;
-    property Key: String read _key write SetKey;
+
+    property ImageEnabled: TPNGObject read _imgEnabled write SetImageEnabled;
+    property ImageSelected: TPNGObject read _imgSelected write SetImageSelected;
 
     property Target: TObject read _target write _target;
 
@@ -57,88 +65,104 @@ uses Types, Windows, TnTWindows;
 
 procedure Register;
 begin
-    RegisterComponents('Win32', [TExGraphicLabel]);
+    RegisterComponents('Win32', [TExGraphicButton]);
 end;
 
-constructor TExGraphicLabel.Create(AOwner: TComponent);
+constructor TExGraphicButton.Create(AOwner: TComponent);
 begin
     _focused := false;
-    
+
+    _imgEnabled := TPNGObject.Create;
+    _imgSelected := TPNGObject.Create;
+
     inherited Create(AOwner);
 end;
 
-procedure TExGraphicLabel.SetCaption(txt: WideString);
+destructor TExGraphicButton.Destroy;
+begin
+    _imgEnabled.Free;
+    _imgSelected.Free;
+
+    inherited Destroy;
+end;
+
+procedure TExGraphicButton.SetCaption(txt: WideString);
 begin
     if txt <> _caption then begin
         _caption := txt;
-        Invalidate;
+        repaint;
     end;
 end;
-procedure TExGraphicLabel.SetFocued(f: Boolean);
-begin
-    if _focused <> f then begin
-        _focused := f;
-        Invalidate;
-    end;
-
-end;
-procedure TExGraphicLabel.SetSelected(sel: Boolean);
+procedure TExGraphicButton.SetSelected(sel: Boolean);
 begin
     if _selected <> sel then begin
         _selected := sel;
-        Invalidate;
+        repaint;
     end;
 end;
-procedure TExGraphicLabel.SetBorder(border: TBorderWidth);
+procedure TExGraphicButton.SetBorder(border: TBorderWidth);
 begin
     if _border <> border then begin
         _border := border;
-        Invalidate;
+        repaint;
     end;
 end;
-procedure TExGraphicLabel.SetKey(key: string);
+procedure TExGraphicButton.SetImageEnabled(img: TPNGObject);
 begin
-  if _key <> key then begin
-    _key := key;
-    Invalidate;
-  end;
+    _imgEnabled.Assign(img);
+    repaint;
+end;
+procedure TExGraphicButton.SetImageSelected(img: TPNGObject);
+begin
+    _imgSelected.Assign(img);
+    repaint;
 end;
 
-procedure TExGraphicLabel.Paint;
+procedure TExGraphicButton.DoEnter;
+begin
+    inherited;
+
+    _focused := true;
+    repaint;
+end;
+procedure TExGraphicButton.DoExit;
+begin
+    inherited;
+
+    _focused := false;
+    repaint;
+end;
+
+procedure TExGraphicButton.Paint;
 var
     OutRect, InRect, fRect: TRect;
     Flags: Longint;
     Text: Widestring;
     Png: TPNGObject;
     Color: TColor;
-    Key: String;
 
 begin
     if Selected then begin
         color := clWhite;
-        key := Self.Key + 'Selected';
+        png := _imgSelected;
     end
     else begin
         color := clBlack;
-        key := Self.Key + 'Enabled';
+        png := _imgEnabled;
     end;
 
     Text := Caption;
+    OutRect := ClientRect;
     InRect := ClientRect;
-    OutRect := InRect;
     InflateRect(InRect, -BorderWidth, -BorderWidth);
     fRect := InRect;
     InflateRect(fRect, -2, -2);
-    
+
     with Canvas do begin
         //paint backround image
-        if not (csDesigning in Self.ComponentState) and (Self.Key <> '') then begin
-            Png := TPngObject.Create();
-            png.LoadFromResourceName(HInstance, key);
-            png.Draw(Canvas, OutRect);
-            png.Free;
-        end;
+        png.Draw(Canvas, OutRect);
 
+        //paint text
         if Text <> '' then begin
             Brush.Style := bsClear;
             Font.Color := Color;
@@ -147,15 +171,28 @@ begin
             Tnt_DrawTextW(Handle, PWideChar(Caption), Length(Caption), InRect, Flags);
         end;
 
+        //paint "focus ring"
         if _focused then begin
+            Pen.Style := psDot;
+            Pen.Color := clBlack;
+            Brush.Style := bsClear;
+            RoundRect(fRect.Left, fRect.Top, fRect.Right, fRect.Bottom, 2, 2);
         end;
 
+        //paint "design ring"
+        {
         if (csDesigning in Self.ComponentState) then begin
             Brush.Style := bsSolid;
             Brush.Color := clBlack;
-            Canvas.FrameRect(inRect);
+            FrameRect(inRect);
         end;
+        }
   end;
+end;
+
+function TExGraphicButton.CanFocus;
+begin
+    Result := inherited CanFocus;
 end;
 
 end.
