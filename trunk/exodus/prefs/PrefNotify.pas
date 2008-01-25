e61aee7c0c0a673b2623953b6fa5dc7e3f66b7b2
joe.fuhrman <joe.fuhrman@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-01-25 00:16:27 +0000

    62050: need default sound for keyword notification
    pref keys are case sensitive

--- trunk/exodus/prefs/PrefNotify.pas
+++ trunk/exodus/prefs/PrefNotify.pas
@@ -273,7 +273,7 @@ begin
     List.add(TNotifyInfo.create(_(sSoundNormalmsg), 'notify_normalmsg'));
     List.add(TNotifyInfo.create(_(sSoundS10n), 'notify_s10n'));
     List.add(TNotifyInfo.create(_(sSoundInvite), 'notify_invite'));
-    List.add(TNotifyInfo.create(_(sSoundKeyword), 'notify_Keyword'));
+    List.add(TNotifyInfo.create(_(sSoundKeyword), 'notify_keyword'));
     List.add(TNotifyInfo.create(_(sSoundChatactivity), 'notify_chatactivity'));
     List.add(TNotifyInfo.create(_(sSoundPriorityChatactivity), 'notify_priority_chatactivity'));
     List.add(TNotifyInfo.create(_(sSoundRoomactivity), 'notify_roomactivity'));
