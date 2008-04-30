a5c756867488d1988aea94dd2bd444527b66dc2a
smorris92 <smorris92@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-04-30 21:02:50 +0000

    Tagged from MomentIM_5.3.0 #36

---
+++ tags/MomentIM_5.3.0-36/exodus/ContactActions.pas
@@ -0,0 +1,170 @@
+unit ContactActions;
+
+interface
+
+uses Exodus_TLB, ExActions;
+
+type
+    TAddContactAction = class(TExBaseAction)
+    private
+        constructor Create;
+
+    public
+        procedure execute(const items: IExodusItemList); override;
+        
+    end;
+
+    TSendContactsAction = class(TExBaseAction)
+    private
+        constructor Create;
+
+    public
+        procedure execute(const items: IExodusItemList); override;
+        
+    end;
+    TBlockContactAction = class(TExBaseAction)
+    private
+        constructor Create;
+
+    public
+        procedure execute(const items: IExodusItemList); override;
+        
+    end;
+    TUnblockContactAction = class(TExBaseAction)
+    private
+        constructor Create;
+
+    public
+        procedure execute(const items: IExodusItemList); override;
+        
+    end;
+
+implementation
+
+uses Classes, ExActionCtrl, ExUtils, gnugettext, JabberID, SelectItem,
+    Session, RosterAdd;
+
+constructor TAddContactAction.Create;
+begin
+    inherited Create('{000-exodus.googlecode.com}-000-add-contact');
+
+    Set_Caption(_('Add Contact'));
+    Set_Enabled(true);
+end;
+procedure TAddContactAction.execute(const items: IExodusItemList);
+begin
+    ShowAddContact();
+end;
+
+constructor TSendContactsAction.Create;
+begin
+    inherited Create('{000-exodus.googlecode.com}-060-send-contacts');
+
+    Set_Caption(_('Send contacts to...'));
+    Set_Enabled(true);
+end;
+
+procedure TSendContactsAction.execute(const items: IExodusItemList);
+var
+    idx: Integer;
+    item: IExodusItem;
+    subjects: TList;
+    target: Widestring;
+begin
+
+    target := SelectUIDByType('contact', _('Select Contacts Recipient'));
+    if (target <> '') then begin
+        subjects := TList.Create;
+
+        for idx := 0 to items.Count - 1 do begin
+            item := items.Item[idx];
+            subjects.Add(Pointer(item));
+        end;
+
+        jabberSendRosterItems(target, subjects);
+    end;
+end;
+
+{
+}
+constructor TBlockContactAction.Create;
+begin
+    inherited Create('{000-exodus.googlecode.com}-080-block-contact');
+
+    Set_Caption(_('Block'));
+    Set_Enabled(true);
+end;
+
+procedure TBlockContactAction.execute(const items: IExodusItemList);
+var
+    idx: Integer;
+    item: IExodusItem;
+    jid: TJabberID;
+begin
+    for idx := 0 to items.Count - 1 do begin
+        item := items.Item[idx];
+        jid := TJabberID.Create(item.UID);
+        MainSession.Block(jid);
+        jid.Free;
+    end;
+end;
+
+{
+}
+constructor TUnblockContactAction.Create;
+begin
+    inherited Create('{000-exodus.googlecode.com}-080-unblock-contact');
+
+    Set_Caption(_('Unblock'));
+    Set_Enabled(true);
+end;
+
+procedure TUnblockContactAction.execute(const items: IExodusItemList);
+var
+    idx: Integer;
+    item: IExodusItem;
+    jid: TJabberID;
+begin
+    for idx := 0 to items.Count - 1 do begin
+        item := items.Item[idx];
+        jid := TJabberID.Create(item.UID);
+        MainSession.UnBlock(jid);
+        jid.Free;
+    end;
+end;
+
+{
+}
+procedure RegisterActions();
+var
+    actCtrl: IExodusActionController;
+    act: IExodusAction;
+begin
+    actCtrl := GetActionController();
+
+    //Setup add contact
+    act := TAddContactAction.Create() as IExodusAction;
+    actCtrl.registerAction('{create}', act);
+
+    //Setup send contacts
+    act := TSendContactsAction.Create() as IExodusAction;
+    actCtrl.registerAction('contact', act);
+
+    //Setup block contact
+    act := TBlockContactAction.Create() as IExodusAction;
+    actCtrl.registerAction('contact', act);
+    actCtrl.addEnableFilter('contact', act.Name, 'blocked=false');
+    actCtrl.addDisableFilter('contact', act.Name, 'selection=multi');
+
+    //Setup unblock contact
+    act := TUnblockContactAction.Create() as IExodusAction;
+    actCtrl.registerAction('contact', act);
+    actCtrl.addEnableFilter('contact', act.Name, 'blocked=true');
+    actCtrl.addDisableFilter('contact', act.Name, 'selection=multi');
+end;
+
+
+initialization
+    RegisterActions();
+
+end.
---
+++ tags/MomentIM_5.3.0-36/exodus/components/ExContactsTreeView.pas
@@ -0,0 +1,60 @@
+{
+    Copyright 2001, Peter Millard
+
+    This file is part of Exodus.
+
+    Exodus is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Exodus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Exodus; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+unit ExContactsTreeView;
+
+interface
+
+uses ExAllTreeView, Exodus_TLB;
+
+
+type
+   TExContactsTreeView = class(TExAllTreeView)
+
+   protected
+       function  FilterItem(Item: IExodusItem): Boolean; override;
+       procedure SaveGroupsState(); override;
+   end;
+
+
+implementation
+
+uses Classes, COMExodusItem;
+
+{---------------------------------------}
+function  TExContactsTreeView.FilterItem(Item: IExodusItem): Boolean;
+begin
+    if (Item.Type_= EI_TYPE_CONTACT) then
+        Result := true
+    else
+        Result := false;
+end;
+
+procedure TExContactsTreeView.SaveGroupsState();
+begin
+
+end;
+
+end.
+
+
+
+
+
