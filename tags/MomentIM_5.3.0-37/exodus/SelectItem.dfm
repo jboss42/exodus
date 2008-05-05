d5082e263d52bf9c1a5324846cfcfbda908c1dec
smorris92 <smorris92@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-05-05 16:29:44 +0000

    Tagged from MomentIM_5.3.0 #37

---
+++ tags/MomentIM_5.3.0-37/exodus/SelectItem.dfm
@@ -0,0 +1,116 @@
+inherited frmSelectItem: TfrmSelectItem
+  Caption = 'Select'
+  ClientHeight = 320
+  ClientWidth = 279
+  ParentFont = False
+  OnCreate = FormCreate
+  ExplicitWidth = 287
+  ExplicitHeight = 354
+  PixelsPerInch = 96
+  TextHeight = 13
+  object pnlInput: TPanel
+    Left = 0
+    Top = 0
+    Width = 279
+    Height = 286
+    Align = alClient
+    BevelOuter = bvNone
+    ParentColor = True
+    TabOrder = 0
+    object pnlSelect: TPanel
+      AlignWithMargins = True
+      Left = 3
+      Top = 3
+      Width = 273
+      Height = 251
+      Align = alClient
+      BevelOuter = bvNone
+      ParentColor = True
+      TabOrder = 0
+    end
+    object pnlEntry: TPanel
+      Left = 0
+      Top = 257
+      Width = 279
+      Height = 29
+      Align = alBottom
+      AutoSize = True
+      BevelOuter = bvNone
+      ParentColor = True
+      TabOrder = 1
+      object lblJID: TTntLabel
+        AlignWithMargins = True
+        Left = 3
+        Top = 3
+        Width = 47
+        Height = 13
+        Align = alLeft
+        Caption = 'Jabber ID'
+        FocusControl = txtJID
+        Layout = tlCenter
+      end
+      object txtJID: TTntEdit
+        AlignWithMargins = True
+        Left = 56
+        Top = 3
+        Width = 220
+        Height = 23
+        Align = alClient
+        TabOrder = 0
+        OnChange = txtJIDChange
+        ExplicitHeight = 21
+      end
+    end
+  end
+  object pnlActions: TPanel
+    Left = 0
+    Top = 286
+    Width = 279
+    Height = 34
+    Align = alBottom
+    AutoSize = True
+    BevelOuter = bvNone
+    ParentColor = True
+    TabOrder = 1
+    object ColorBevel1: TColorBevel
+      Left = 0
+      Top = 0
+      Width = 279
+      Height = 2
+      Align = alTop
+      HighLight = clBtnHighlight
+      Shadow = clBtnShadow
+      FrameColor = frDefault
+      ExplicitWidth = 281
+    end
+    object btnCancel: TTntButton
+      AlignWithMargins = True
+      Left = 203
+      Top = 6
+      Width = 75
+      Height = 25
+      Caption = 'Cancel'
+      ModalResult = 2
+      TabOrder = 0
+    end
+    object btnOK: TTntButton
+      AlignWithMargins = True
+      Left = 122
+      Top = 6
+      Width = 75
+      Height = 25
+      Caption = 'OK'
+      Enabled = False
+      ModalResult = 1
+      TabOrder = 1
+    end
+  end
+  object popSelected: TTntPopupMenu
+    Left = 56
+    Top = 88
+    object mnuShowOnline: TTntMenuItem
+      Caption = 'Show Online Only'
+      OnClick = mnuShowOnlineClick
+    end
+  end
+end
---
+++ tags/MomentIM_5.3.0-37/jopl/RoomController.pas
@@ -0,0 +1,340 @@
+{
+    Copyright 2001, Peter Millard
+
+    This file is part of Exodus.
+
+    Exodus is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Exodus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Exodus; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+unit RoomController;
+
+interface
+uses XMLTag, Exodus_TLB, Unicode, ComObj;
+
+type TRoomController = class
+     private
+         _JS: TObject;
+         _SessionCB: Integer;
+         _ItemsCB: IExodusItemCallback;
+
+         //Methods
+         procedure _GetRooms();
+         procedure _ParseRooms(Event: string; Tag: TXMLTag);
+         procedure _ParseRoom(Room: IExodusItem; Tag: TXMLTag);
+         procedure _SessionCallback(Event: string; Tag: TXMLTag);
+     public
+         constructor Create(JS: TObject);
+         destructor  Destroy; override;
+         procedure   AddRoom(const JabberID, RoomName, Nickname: WideString;
+                                 AutoJoin, UseRegisteredNick: Boolean;
+                                 Groups: TWideStringList);
+         procedure RemoveRoom(const JabberID: WideString);
+         procedure SaveRooms();
+
+end;
+
+type TExodusRoomsCallback = class(TAutoIntfObject, IExodusItemCallback)
+    private
+        _roomCtrl: TRoomController;
+
+        constructor Create(rc: TRoomController);
+
+    public
+        destructor Destroy(); override;
+
+        procedure ItemDeleted(const item: IExodusItem); safecall;
+        procedure ItemGroupsChanged(const item: IExodusItem); safecall;
+end;
+
+implementation
+uses Session, IQ, JabberConst, SysUtils, COMExodusItem, JabberID, RosterImages,
+     DisplayName, ComServ;
+
+{---------------------------------------}
+constructor TRoomController.Create(JS: TObject);
+begin
+    _JS := JS;
+    _SessionCB := TJabberSession(_JS).RegisterCallback(_SessionCallback, '/session');
+    _ItemsCB := TExodusRoomsCallback.Create(Self);
+end;
+
+{---------------------------------------}
+destructor  TRoomController.Destroy();
+begin
+    with TJabberSession(_js) do begin
+        UnregisterCallback(_SessionCB);
+    end;
+
+    _ItemsCB := nil;
+end;
+
+{---------------------------------------}
+procedure TRoomController.AddRoom(const JabberID, RoomName, Nickname: WideString;
+                                  AutoJoin, UseRegisteredNick: Boolean; Groups: TWideStringList);
+var
+    Room: IExodusItem;
+    i: Integer;
+begin
+    Room := TJabberSession(_js).ItemController.AddItemByUid(JabberID, EI_TYPE_ROOM, _ItemsCB);
+    Room.ImageIndex := RI_CONFERENCE_INDEX;
+    Room.Active := true;
+    Room.Text := RoomName;    
+    Room.AddProperty('name', RoomName);
+    Room.AddProperty('nick', Nickname);
+    if (AutoJoin) then
+        Room.AddProperty('autojoin', 'true')
+    else
+        Room.AddProperty('autojoin', 'false');
+        
+    if (UseRegisteredNick) then
+        Room.AddProperty('reg_nick', 'true')
+    else
+        Room.AddProperty('reg_nick', 'false');
+
+    if (Groups = nil) then
+    begin
+       Groups := TWideStringList.Create();
+       Groups.Add(TJabberSession(_js).Prefs.getString('roster_default'));
+    end;
+
+    for i := 0 to Groups.Count - 1 do
+    begin
+        Room.AddGroup(Groups[i]);
+    end;
+      
+
+    TJabberSession(_JS).FireEvent('/item/add', Room);
+    SaveRooms();
+end;
+
+{---------------------------------------}
+procedure TRoomController.RemoveRoom(const JabberID: WideString);
+var
+    Room: IExodusItem;
+begin
+    Room :=  TJabberSession(_js).ItemController.GetItem(JabberID);
+    TJabberSession(_JS).FireEvent('/item/remove', Room);
+    TJabberSession(_js).ItemController.RemoveItem(JabberID);
+    SaveRooms();
+end;
+
+{---------------------------------------}
+procedure TRoomController.SaveRooms();
+var
+    i,j: Integer;
+    IQ, RoomTag, GroupTag, StorageTag:TXMLTag;
+    Room: IExodusItem;
+begin
+    IQ := TXMLTag.Create('iq');
+    with IQ do begin
+        setAttribute('type', 'set');
+        setAttribute('id', TJabberSession(_js).generateID());
+        with AddTag('query') do begin
+            setAttribute('xmlns', XMLNS_PRIVATE);
+            StorageTag := AddTag('storage');
+            StorageTag.setAttribute('xmlns', XMLNS_BM);
+            for i := 0 to TJabberSession(_js).ItemController.ItemsCount - 1 do
+            begin
+                if (TJabberSession(_js).ItemController.Item[i].Type_ <> EI_TYPE_ROOM) then continue;
+                Room := TJabberSession(_js).ItemController.Item[i];
+                RoomTag := StorageTag.AddTag('conference');
+                RoomTag.setAttribute('xmlns', XMLNS_BM);
+                RoomTag.setAttribute('jid', Room.uid);
+                RoomTag.setAttribute('name', Room.Value['name']);
+                RoomTag.setAttribute('autojoin', Room.Value['autojoin']);
+                RoomTag.setAttribute('reg_nick', Room.Value['reg_nick']);
+                RoomTag.AddBasicTag('nick', Room.Value['nick']);
+                RoomTag.AddBasicTag('password', Room.Value['password']);
+                for j := 0 to Room.GroupCount - 1 do
+                begin
+                    GroupTag := RoomTag.AddBasicTag('group', Room.Group[j]);
+                    GroupTag.setAttribute('xmlns', 'http://jabber.com/protocols');
+                end;
+            end;
+        end;
+    end;
+    TJabberSession(_JS).SendTag(IQ);
+end;
+
+{---------------------------------------}
+procedure TRoomController._SessionCallback(Event: string; Tag: TXMLTag);
+begin
+     if Event = '/session/authenticated'  then
+     begin
+         _GetRooms();
+     end
+end;
+
+{---------------------------------------}
+//Creates and sends out an IQ to retrieve
+//bookmarks from the server.
+procedure TRoomController._GetRooms();
+var
+    IQ: TJabberIQ;
+    Session: TJabberSession;
+begin
+    Session := TJabberSession(_js);
+    IQ := TJabberIQ.Create(Session, Session.generateID(), _ParseRooms, 180);
+    with iq do begin
+        iqType := 'get';
+        toJid := '';
+        Namespace := XMLNS_PRIVATE;
+        with qtag.AddTag('storage') do
+            setAttribute('xmlns', XMLNS_BM);
+        Send();
+    end;
+end;
+
+{---------------------------------------}
+//Parses the xml with bookmarks received from the server.
+procedure TRoomController._ParseRooms(Event: string; Tag: TXMLTag);
+var
+    RoomTags: TXMLTagList;
+    i: integer;
+    StorageTag, RoomTag: TXMLTag;
+    jid: Widestring;
+    TmpJID: TJabberID;
+    Item: IExodusItem;
+begin
+    Item := nil;
+    TJabberSession(_JS).FireEvent('/item/begin', Item);
+    RoomTags := nil;
+    if ((Event = 'xml') and (Tag.getAttribute('type') <> 'result')) then exit;
+
+    // We got a response..
+    StorageTag := tag.QueryXPTag('/iq/query/storage');
+    if (StorageTag <> nil) then
+        RoomTags := StorageTag.ChildTags();
+
+    for i := 0 to RoomTags.Count - 1 do begin
+            if (RoomTags[i].Name <> 'conference') then continue;
+            RoomTag := RoomTags.Tags[i];
+            jid := WideLowerCase(RoomTag.GetAttribute('jid'));
+            TmpJID := TJabberID.Create(RoomTag.GetAttribute('jid'));
+            Item := TJabberSession(_js).ItemController.AddItemByUid(TmpJID.full, EI_TYPE_ROOM, _ItemsCB);
+            //Make sure item exists
+            if (Item <> nil) then
+            begin
+                _ParseRoom(Item, RoomTag);
+
+                if (Item.IsVisible) then
+                    TJabberSession(_JS).FireEvent('/item/add', Item);
+            end;
+            TmpJID.Free();
+     end;
+
+     Item := nil;
+     TJabberSession(_JS).FireEvent('/item/end', Item);
+     TJabberSession(_JS).FireEvent('/data/item/group/restore', nil, '');
+     RoomTags.Free();
+
+end;
+
+{---------------------------------------}
+//Sets some specific and generic properties of
+//IExodusItem interface based on the tag data.
+procedure TRoomController._ParseRoom(Room: IExodusItem; Tag: TXMLTag);
+var
+    TmpJid: TJabberID;
+    Grps: TXMLTagList;
+    TmpTag: TXMLTag;
+    i: Integer;
+    Grp, Groups: WideString;
+begin
+    {
+        <iq type="set" id="jcl_4">
+            <query xmlns="jabber:iq:private">
+                <storage xmlns="storage:bookmarks">
+                    <conference name='Council of Oberon'
+                                  autojoin='true'
+                                  jid='council@conference.underhill.org'>
+                        <nick>Puck</nick>
+                        <group ns="http://jabber.com/protocols">abc</group>
+                    </conference>
+                </storage>
+        </query></iq>
+        }
+    Room.ImageIndex := RI_CONFERENCE_INDEX;
+    TmpJid := TJabberID.Create(Tag.GetAttribute('jid'));
+    Room.AddProperty('name', Tag.GetAttribute('name'));
+    //Retrieve room name from display cache
+    GetDisplayNameCache().UpdateDisplayName(Room);
+    Room.Text := GetDisplayNameCache().GetDisplayName(Room.Uid);
+    Room.AddProperty('autojoin', Tag.GetAttribute('autojoin'));
+    Room.AddProperty('reg_nick', Tag.GetAttribute('reg_nick'));
+    TmpTag := Tag.QueryXPTag('/conference/nick');
+    if (TmpTag <> nil) then
+        Room.AddProperty('nick', TmpTag.Data);
+    TmpTag := Tag.QueryXPTag('/conference/password');
+    if (TmpTag <> nil) then
+        Room.AddProperty('password', TmpTag.Data);
+
+    Grps := Tag.QueryXPTags('/conference/group');
+    //Build temporary list of groups for future comparison of the lists.
+    for i := 0 to Grps.Count - 1 do
+    begin
+        Grp := WideTrim(TXMLTag(grps[i]).Data);
+        Groups := Groups + Grp + LineSeparator;
+    end;
+
+    if (Room.GroupsChanged(Groups)) then
+    begin
+    //If groups changed, update the list.
+        Room.ClearGroups();
+        for i := 0 to Grps.Count - 1 do
+        begin
+            Grp := WideTrim(TXMLTag(Grps[i]).Data);
+            if (Grp <> '') then
+            begin
+                Room.AddGroup(grp);
+                //TJabberSession(_js).ItemController.AddItemByUID(grp, EI_TYPE_GROUP);
+            end;
+
+        end;
+    end;
+
+    if (Room.GroupCount = 0) then
+    begin
+        Room.AddGroup(TJabberSession(_JS).Prefs.getString('roster_default'));
+    end;
+
+   
+    Grps.Free();
+    TmpJid.Free();
+end;
+
+constructor TExodusRoomsCallback.Create(rc: TRoomController);
+begin
+    inherited Create(ComServer.TypeLib, IID_IExodusItemCallback);
+
+    _roomCtrl := rc;
+end;
+destructor TExodusRoomsCallback.Destroy();
+begin
+    inherited;
+end;
+
+procedure TExodusRoomsCallback.ItemDeleted(const item: IExodusItem);
+begin
+    _roomCtrl.SaveRooms();
+    item.IsVisible := false;
+    TJabberSession(_roomCtrl._JS).FireEvent('/item/remove', item);
+end;
+procedure TExodusRoomsCallback.ItemGroupsChanged(const item: IExodusItem);
+begin
+    _roomCtrl.SaveRooms();
+    TJabberSession(_roomCtrl._JS).FireEvent('/item/update', item);
+end;
+
+end.
