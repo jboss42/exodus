0f54bf4dbbffa4860ba0126be17892bccab859c1
joe.fuhrman <joe.fuhrman@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-05-05 15:54:39 +0000

    tagging for SCM build

---
+++ tags/SCM_build_tag/exodus/COMExodusTab.pas
@@ -0,0 +1,232 @@
+{
+    Copyright 2001, Peter Millard
+
+    This file is part of Exodus.
+
+    Exodus is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Exodus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Exodus; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+unit COMExodusTab;
+
+{$WARN SYMBOL_PLATFORM OFF}
+
+interface
+
+uses
+  ComObj, ActiveX, Exodus_TLB, StdVcl, PLUGINCONTROLLib_TLB, TntComCtrls;
+
+type
+  TExodusTab = class(TAutoObject, IExodusTab)
+  protected
+      function Get_Caption: WideString; safecall;
+      function Get_ImageIndex: Integer; safecall;
+      function Get_Name: WideString; safecall;
+      function Get_Visible: WordBool; safecall;
+      procedure Hide; safecall;
+      procedure Set_Caption(const Value: WideString); safecall;
+      procedure Set_ImageIndex(Value: Integer); safecall;
+      procedure Set_Name(const Value: WideString); safecall;
+      procedure Show; safecall;
+      function Get_Handle: Integer; safecall;
+      function Get_UID: WideString; safecall;
+      function Get_Height: Integer; safecall;
+      function Get_Width: Integer; safecall;
+      function Get_Description: WideString; safecall;
+      function Get_AXControl: OleVariant; safecall;
+      procedure Set_Description(const value: WideString); safecall;
+      function Get_PageIndex: Integer; safecall;
+      function Get_TabIndex: Integer; safecall;
+      function GetSelectedItems: IExodusItemList; safecall;
+  private
+      _AxControl: TAXControl;
+      _Page: TTntTabSheet;
+      _UID:  WideString;
+      _Name: WideString;
+      _Desc: WideString;
+  public
+      constructor Create(ActiveX_GUID: WideString);
+      destructor Destroy; override;
+  end;
+
+implementation
+
+uses ComServ, RosterForm, SysUtils, Session, Variants, Controls, COMExodusItemList;
+
+{---------------------------------------}
+constructor TExodusTab.Create(ActiveX_GUID: WideString);
+var
+    g: TGUID;
+begin
+    _AxControl := nil;
+    _Page := TTntTabSheet.Create(GetRosterWindow()._PageControl);
+    _Page.PageControl :=  GetRosterWindow()._PageControl;
+    _Page.TabVisible := true;
+    _UID := ActiveX_GUID;
+    if (_UID <> '') then
+    begin
+        _AxControl := TAXControl.Create(_Page, StringToGuid(_UID));
+        _AxControl.Parent := _Page;
+        _AxControl.Align := alClient;
+    end
+    else
+    begin
+        CreateGUID(g);
+        _UID := GUIDToString(g);
+    end;
+end;
+
+{---------------------------------------}
+destructor TExodusTab.Destroy();
+begin
+    _AxControl.Free;
+    _Page.Free;
+    inherited;
+end;
+
+{---------------------------------------}
+function TExodusTab.Get_Caption: WideString;
+begin
+    Result := _Page.Caption;
+end;
+
+{---------------------------------------}
+function TExodusTab.Get_ImageIndex: Integer;
+begin
+   Result := _Page.ImageIndex;
+end;
+
+{---------------------------------------}
+function TExodusTab.Get_Name: WideString;
+begin
+    Result := _Name;
+end;
+
+{---------------------------------------}
+function TExodusTab.Get_Visible: WordBool;
+begin
+    Result := _Page.TabVisible;
+end;
+
+{---------------------------------------}
+procedure TExodusTab.Hide;
+begin
+   _Page.TabVisible := false;
+   MainSession.FireEvent('/session/tab/hide', nil);
+end;
+
+{---------------------------------------}
+procedure TExodusTab.Set_Caption(const Value: WideString);
+begin
+    _Page.Caption := Value;
+end;
+
+{---------------------------------------}
+procedure TExodusTab.Set_ImageIndex(Value: Integer);
+begin
+    _Page.ImageIndex := Value;
+end;
+
+procedure TExodusTab.Set_Name(const Value: WideString);
+begin
+    _Name := Value;
+end;
+
+{---------------------------------------}
+procedure TExodusTab.Show;
+begin
+    _Page.TabVisible := true;
+    MainSession.FireEvent('/session/tab/show', nil);
+end;
+
+{---------------------------------------}
+function TExodusTab.Get_Handle: Integer;
+begin
+    Result := _Page.Handle;
+end;
+
+{---------------------------------------}
+function TExodusTab.Get_UID: WideString;
+begin
+   Result := _UID;
+end;
+
+function TExodusTab.Get_Height: Integer;
+begin
+    Result := _Page.Height;
+end;
+
+function TExodusTab.Get_Width: Integer;
+begin
+   Result := _Page.Width;
+end;
+
+
+function TExodusTab.Get_Description: WideString;
+begin
+    Result := _Desc;
+end;
+
+
+function TExodusTab.Get_AXControl: OleVariant;
+begin
+   Result := unassigned;;
+   if (_AXControl <> nil) then
+       Result := _AXControl.OleObject;
+   
+end;
+
+procedure TExodusTab.Set_Description(const value: WideString);
+begin
+   _Desc := Value;
+end;
+
+function TExodusTab.Get_PageIndex: Integer;
+begin
+    Result := _Page.PageIndex;
+end;
+
+function TExodusTab.Get_TabIndex: Integer;
+begin
+   Result := _Page.TabIndex;
+end;
+
+function TExodusTab.GetSelectedItems: IExodusItemList;
+var
+   Selection: IExodusItemSelection;
+begin
+   Result := nil;
+
+   if (VarIsEmpty(Get_AXControl())) then
+   begin
+      Result := GetRosterWindow().SelectionFor(Get_PageIndex);
+   end
+   else
+   begin
+     try
+        Selection := IUnknown(Get_AXControl()) as  IExodusItemSelection;
+        Result := Selection.GetSelectedItems();
+     except
+         on EIntfCastError do begin
+            //TODO:  loggit??
+         end;
+     end;
+   end;
+
+   if (Result = nil) then Result := TExodusItemList.Create();
+end;
+
+initialization
+  TAutoObjectFactory.Create(ComServer, TExodusTab, Class_ExodusTab,
+    ciMultiInstance, tmApartment);
+end.
---
+++ tags/SCM_build_tag/exodus/SelectItem.pas
@@ -0,0 +1,263 @@
+unit SelectItem;
+{
+    Copyright 2008, Estate of Peter Millard
+
+    This file is part of Exodus.
+
+    Exodus is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Exodus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Exodus; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, ComCtrls, ExForm,
+  Forms, ExTreeView, Exodus_TLB, Dialogs, ExtCtrls, StdCtrls, TntStdCtrls,
+  Menus, TntMenus, ExGradientPanel, SClrRGrp;
+
+type
+  TfrmSelectItem = class;
+
+  TTypedTreeView = class(TExTreeView)
+  private
+    _online: Boolean;
+    _itemtype: Widestring;
+
+    constructor Create(AOwner: TfrmSelectItem; Session: TObject);
+
+    procedure SetShowOnline(flag: Boolean);
+    procedure SetItemType(itemtype: Widestring);
+
+  protected
+    procedure SaveGroupsState(); override;
+    function FilterItem(item: IExodusItem): Boolean; override;
+
+  public
+    destructor Destroy; override;
+
+    procedure DblClick; override;
+
+    property ShowOnline: Boolean read _online write SetShowOnline;
+    property ItemType: Widestring read _itemtype write SetItemType;
+  end;
+
+  TfrmSelectItem = class(TExForm)
+    pnlInput: TPanel;
+    pnlActions: TPanel;
+    pnlSelect: TPanel;
+    pnlEntry: TPanel;
+    btnCancel: TTntButton;
+    btnOK: TTntButton;
+    txtJID: TTntEdit;
+    lblJID: TTntLabel;
+    popSelected: TTntPopupMenu;
+    mnuShowOnline: TTntMenuItem;
+    ColorBevel1: TColorBevel;
+    
+    procedure FormCreate(Sender: TObject);
+
+    procedure ItemChanged(Sender: TObject; Node: TTreeNode);
+    procedure mnuShowOnlineClick(Sender: TObject);
+    procedure txtJIDChange(Sender: TObject);
+
+  private
+    { Private declarations }
+
+  protected
+    { Protected declarations }
+    _itemType: Widestring;
+    _selectedUID: Widestring;
+    _itemView: TTypedTreeView;
+
+    constructor Create(AOwner: TComponent; itemtype: Widestring);
+
+  public
+    { Public declarations }
+    destructor Destroy; override;
+
+    Property ItemType: Widestring read _itemType;
+    Property SelectedUID: Widestring read _selectedUID;
+
+  end;
+
+function SelectUIDByType(itemtype: Widestring; title: Widestring = ''): Widestring;
+
+
+implementation
+
+uses
+    gnugettext,
+    Session,
+    JabberID,
+    COMExodusItem;
+
+{$R *.dfm}
+
+function SelectUIDByType(itemtype: Widestring; title: Widestring): Widestring;
+var
+    selector: TfrmSelectItem;
+begin
+    Result := '';
+    selector := TfrmSelectItem.Create(nil, itemtype);
+    if (title <> '') then
+        selector.Caption := title;
+
+    if (selector.ShowModal = mrOk) then begin
+        Result := selector.SelectedUID;
+    end;
+
+    selector.Free;
+end;
+
+constructor TTypedTreeView.Create(AOwner: TfrmSelectItem; Session: TObject);
+begin
+    inherited Create(AOwner, Session);
+end;
+destructor TTypedTreeView.Destroy;
+begin
+    inherited;
+end;
+
+procedure TTypedTreeView.SetShowOnline(flag: Boolean);
+begin
+    if (flag <> _online) then begin
+        _online := flag;
+
+        if not (csLoading in ComponentState) and (Parent <> nil) then
+            Refresh();
+    end;
+end;
+procedure TTypedTreeView.SetItemType(itemtype: WideString);
+begin
+    if (itemtype <> _itemtype) then begin
+        _itemtype := itemtype;
+
+        if not (csLoading in ComponentState) and (Parent <> nil) then
+            Refresh();
+    end;
+end;
+
+procedure TTypedTreeView.SaveGroupsState;
+begin
+    //DO NOTHING!
+end;
+function TTypedTreeView.FilterItem(item: IExodusItem): Boolean;
+begin
+    Result := Item.IsVisible or (not ShowOnline);
+    if Result then begin
+        Result := (ItemType = '') or (item.Type_ = 'group') or (ItemType = item.Type_);
+    end;
+end;
+
+procedure TTypedTreeView.DblClick;
+begin
+    if (Selected = nil) or (IExodusItem(Selected.Data).Type_ = EI_TYPE_GROUP) then begin
+        inherited;
+        exit;
+    end;
+
+    with TfrmSelectItem(Owner) do begin
+        ItemChanged(Self, Selected);
+        ModalResult := mrOk;
+        Hide;
+    end;
+end;
+
+{
+}
+constructor TfrmSelectItem.Create(AOwner: TComponent; itemtype: Widestring);
+begin
+    inherited Create(AOwner);
+
+    _itemtype := itemtype;
+    _selectedUID := '';
+end;
+
+destructor TfrmSelectItem.Destroy;
+begin
+    _itemView.Free;
+    _itemView := nil;
+
+    inherited;
+end;
+
+procedure TfrmSelectItem.FormCreate(Sender: TObject);
+begin
+    inherited;
+
+    Self.Caption := _(Self.Caption + ' ' + _itemtype);
+
+    _itemView := TTypedTreeView.Create(Self, MainSession);
+    _itemView.ItemType := _itemtype;
+    _itemView.Parent := pnlSelect;
+    _itemView.MultiSelect := false;
+    _itemView.Align := alClient;
+    _itemView.AlignWithMargins := false;
+    _itemView.OnChange := ItemChanged;
+    _itemView.PopupMenu := popSelected;
+    _itemView.BorderStyle := bsSingle;
+    with MainSession.Prefs do begin
+        _itemView.Font.Name := getString('roster_font_name');
+        _itemView.Font.Size := getInt('roster_font_size');
+        _itemView.Font.Color := TColor(getInt('roster_font_color'));
+        _itemView.Font.Charset := getInt('roster_font_charset');
+        _itemView.ShowOnline := getBool('roster_only_online');
+    end;
+    mnuShowOnline.Checked := _itemView.ShowOnline;
+
+    _itemView.Refresh;
+end;
+
+procedure TfrmSelectItem.ItemChanged(Sender: TObject; Node: TTreeNode);
+var
+    item: IExodusItem;
+    valid: Boolean;
+begin
+    Item := _itemView.GetNodeItem(Node);
+    valid := (Item <> nil) and (Item.Type_ = _itemtype);
+    if not valid then begin
+        _selectedUID := '';
+        txtJID.text := '';
+    end
+    else begin
+        _selectedUID := item.UID;
+        txtJID.text := item.UID;
+    end;
+
+    btnOK.Enabled := valid;
+end;
+
+procedure TfrmSelectItem.mnuShowOnlineClick(Sender: TObject);
+begin
+    mnuShowOnline.Checked := not mnuShowOnline.Checked;
+    _itemView.ShowOnline := mnuShowOnline.Checked;
+end;
+
+procedure TfrmSelectItem.txtJIDChange(Sender: TObject);
+begin
+    inherited;
+
+    if (isValidJID(txtJID.Text, false)) then begin
+        _selectedUID := txtJID.Text;
+        btnOK.Enabled := true;
+    end
+    else begin
+        btnOK.Enabled := false;
+    end;
+end;
+
+
+
+end.
