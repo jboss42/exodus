957a1a8068d97a4823faa73f798a77b26a9a3104
linuxwolf <linuxwolf@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-06-30 15:19:12 +0000

    forward-porting session jid stuff from 5.2.1

--- jopl/Session.pas
+++ jopl/Session.pas
@@ -209,6 +209,7 @@ type
         property Priority: integer read _priority write _priority;
         property Show: WideString read _show;
         property Status: WideString read _status;
+        property SessionJid: TJabberID read GetSessionJid;
 
         // Stream stuff
         property Stream: TXMLStream read _stream;
