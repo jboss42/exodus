051274d8e1d2e2caa7d96bc8eb694812ef02cd86
joe.fuhrman <joe.fuhrman@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-07-30 13:12:38 +0000

    5.4.0 release branch

---
+++ branches/5.4.0/trunk/exodus/components/ExHoverFrame.pas
@@ -0,0 +1,21 @@
+unit ExHoverFrame;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms, 
+  Dialogs, ExFrame;
+
+type
+  TExHoverFrame = class(TExFrame)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+end.
