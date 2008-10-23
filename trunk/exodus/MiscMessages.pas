cf6f6a8ef5c16f756b7fe9273c251c0e1a4ea2ee
dabbink <dabbink@29b8745e-4034-0410-bb18-b3d48ac8bcd4>
2008-10-23 18:57:10 +0000

    Changed what event queue Headline messages listen on so that PEP messages don't trigger a new chat window.

--- trunk/exodus/MiscMessages.pas
+++ trunk/exodus/MiscMessages.pas
@@ -398,7 +398,8 @@ end;
 *******************************************************************************}
 Constructor THeadlineHandler.Create();
 begin
-    Create('/packet/message[@type="headline"]');
+    //Create('/packet/message[@type="headline"]');
+    Create('/post/message[@type="headline"]');
 end;
 
 procedure THeadlineHandler.MessageCallback(event: string; tag: TXMLTag);
